ce0947fd0ccbc9edaf744c1d31b5450925cab83f
Kenneth Cox <damitwhy@gmail.com>
2024-08-24 01:04:08 +0000

    Working bug fixed

--- kc_resume/settings.py
+++ kc_resume/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #os.environ.get("DEBUG", "") == "1"
+DEBUG = os.environ.get("DEBUG", "") == "1"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
                  '.ws.codeinstitute-ide.net', '.herokuapp.com']
